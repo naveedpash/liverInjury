6cb7866b122dac5f31bf81f0a7787fcfe324d57e
Naveed Aman Pasha <naveedpash@gmail.com>
2018-11-05 07:57:45 +0000

    removed redandant files

--- app/components/Container/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var Container_1 = __importDefault(require("./Container"));
-exports.Container = Container_1.default;
-var styles_1 = __importDefault(require("./styles"));
-exports.styles = styles_1.default;
