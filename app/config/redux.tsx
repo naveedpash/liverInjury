26966ff818dac885472533980a1de760721fc09a
Naveed Aman Pasha <naveedpash@gmail.com>
2018-11-25 20:10:26 +0500

    first commit for R Shiny App

--- app/config/redux.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import { reducer as formReducer } from "redux-form";
-
-const reducers = {
-    form: formReducer,
-}
-const reducer = combineReducers(reducers);
-export default createStore(reducer);
