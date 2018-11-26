08c7f69bdd3783b67dcc4cc2330dfa088f3e70fd
Naveed Aman Pasha <naveedpash@gmail.com>
2018-11-26 22:50:40 +0500

    gathered firebase data and reshaped into dataframe

--- server.R
+++ server.R
@@ -1,15 +1,19 @@
 library(fireData)
+library(plyr)
+library(jsonlite)
+library(tidyr)
+library(dplyr)
+library(magrittr)
 
 token <- auth("AIzaSyCP12tkAAs7L8ScltoGAsiKW-EB5z1BBJc",
               "naveedpash@gmail.com",
               "evolve@2009")
 followup <- download(projectURL = "https://dili-16490.firebaseio.com/",
-                     fileName = "followup/",
-                     token = token$idToken)
+    fileName = "followup/",
+    token = token$idToken) %>% ldply(stack) %>% spread(ind, values)
 mortality <- download(projectURL = "https://dili-16490.firebaseio.com/",
-                     fileName = "mortality/",
-                     token = token$idToken)
+    fileName = "mortality/",
+    token = token$idToken) %>% ldply(stack) %>% spread(ind, values) %>% mutate(.id = nic)
 
 server <- function(input, output) {
-    
 }
