ba0121b48eb5992248d54dff737fbcae368d7054
Naveed Aman Pasha <naveedpash@gmail.com>
2018-12-06 22:53:50 +0500

    more data munging

--- server.R
+++ server.R
@@ -14,25 +14,29 @@ followup <- download(projectURL = "https://dili-16490.firebaseio.com/",
     fileName = "followup/",
     token = token$idToken) %>% ldply(stack) %>% spread(ind, values)
 mortality <- download(projectURL = "https://dili-16490.firebaseio.com/",
-    fileName = "mortality/",
-    token = token$idToken) %>% ldply(stack) %>% spread(ind, values) %>% mutate(.id = nic)
+                      fileName = "mortality/",
+                      token = token$idToken) %>%
+    ldply(stack) %>% spread(ind, values) %>% mutate(.id = nic)
 
 server <- function(input, output) {
-    data <- as.data.set(spss.system.file("data/farhana.sav"))
+    data <- memisc::as.data.set(memisc::spss.system.file("data/farhana.sav"))
     data <- as.data.frame(data)
     data$drugname <- data$nameofdrug
     data$drugdose <- data$doseofthedrug
     data %>%
-        mutate(drugname = case_when(str_detect(drugname,"\\(+")                           ~ str_extract(drugname, "(?<=\\().+?(?=\\))"),
-                                    str_detect(drugname,"(?<!(m|M)yrin(-| )(p|P))/ATT$")  ~ str_replace(drugname, "/ATT", ""),
-                                    str_detect(drugname,"^ATT/(?!(m|M)yrin(-| )(p|P))")   ~ str_replace(drugname, "ATT/", ""),
-                                    str_detect(drugname,"^((m|M)yrin(-| )(p|P))/(?!ATT)") ~ str_replace(drugname, "((m|M)yrin(-| )(p|P))/", ""),
-                                    TRUE ~ as.character(drugname))) -> data
-    for(i in 1:nrow(data)) {
-        if(length(str_split(data$drugname, "/")[[i]]) != length(str_split(data$drugdose, "/")[[i]])) {
-            print(data[i,c('drugname','drugdose')])
-        }
+        mutate(drugname = case_when(str_detect(drugname, "\\(+")                           ~ str_extract(drugname, "(?<=\\().+?(?=\\))"),
+                                    str_detect(drugname, "(?<!(m|M)yrin(-| )(p|P))/ATT$")  ~ str_replace(drugname, "/ATT", ""),
+                                    str_detect(drugname, "^ATT/(?!(m|M)yrin(-| )(p|P))")   ~ str_replace(drugname, "ATT/", ""),
+                                    str_detect(drugname, "^((m|M)yrin(-| )(p|P))/(?!ATT)") ~ str_replace(drugname, "((m|M)yrin(-| )(p|P))/", ""),
+                                    TRUE ~ as.character(drugname))) %>%
+        filter(drugname != "") -> data
+    disorganised <- vector(mode = "logical")
+    for (i in 1:nrow(data)) {
+        disorganised[i] <- (length(stringr::str_split(data$drugname, "/")[[i]]) != length(stringr::str_split(data$drugdose, "/")[[i]]))
     }
+    blanks <- data$drugdose == ""
+    drugsDoses <- data %>% filter(!disorganised & !blanks) %>% separate_rows(drugname, drugdose, sep="/")
+    drugsWoDoses <- data %>% filter(!disorganised & blanks) %>% separate_rows(drugname, sep="/")
     data %>% filter(length(unlist(str_split(drugname, "/"))) != length(unlist(str_split(drugdose, "/"))))
         select(nameofdrug, drugname, drugdose) -> data
 }
