ebf93eb833e18f423ce2e38682fe05175c979470
Naveed Aman Pasha <naveedpash@gmail.com>
2018-12-12 22:36:28 +0500

    needs styling

--- ui.R
+++ ui.R
@@ -3,27 +3,30 @@ library(dplyr)
 drugsData <- read.csv2("data/drugsData.csv")
 drugnames <- drugsData %>% count(drugname, sort=TRUE) %>% filter(n>10) %>% select(drugname)
 
-ui <- fluidPage(
-    titlePanel("Karachi Registry for Drug Induced Liver Injury"),
-    headerPanel("Summary of Suspected Medications"),
-    sidebarPanel(
-                checkboxGroupInput(inputId = "drugs",
-                                   choices = drugnames,
-                                   label = "Medication"
-                                   )
-                 ),
-    mainPanel(
-        tabsetPanel(type="tabs",
-            tabPanel("Summary",
-                     plotOutput("drugsdrugsBarchart", width="500px", height="400px")
-            ),
-            tabPanel("Morbitidy",
-                        plotOutput("tbPlot", width="500px", height="400px"),
-                        plotOutput("sgptPlot", width="500px", height="400px"),
-                        plotOutput("apPlot", width="500px", height="400px"),
-                        plotOutput("ggtPlot", width="500px", height="400px"),
-                        plotOutput("ptPlot", width="500px", height="400px")
-            )
-        ),
+ui <- shinyUI(
+              fluidPage(theme = "styles.css",
+                        titlePanel("Karachi Registry for Drug Induced Liver Injury"),
+                        mainPanel(
+                                  tabsetPanel(type = "tabs",
+                                              tabPanel("Summary",
+                                                       headerPanel("Summary of Suspected Medications"),
+                                                       plotOutput("drugsBarchart", width="500px", height="400px")
+                                                       ),
+                                              tabPanel("Morbidity",
+                                                       headerPanel("Effect of Suspected Medications on LFTs"),
+                                                       sidebarPanel(
+                                                                    checkboxGroupInput(inputId = "drugs",
+                                                                                       choices = drugnames$drugname,
+                                                                                       label = "Medication"
+                                                                                       )
+                                                                    ),
+                                                       plotOutput("tbPlot", width="500px", height="400px"),
+                                                       plotOutput("sgptPlot", width="500px", height="400px"),
+                                                       plotOutput("apPlot", width="500px", height="400px"),
+                                                       plotOutput("ggtPlot", width="500px", height="400px"),
+                                                       plotOutput("ptPlot", width="500px", height="400px")
+                                                       )
+                                              )
+                                  )
+              )
     )
-)
